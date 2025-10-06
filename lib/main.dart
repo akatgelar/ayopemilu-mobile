efdcd7806ad74de645ca73de5a2f0e84931b2923
akatgelar <akatgelar@gmail.com>
2025-10-06 13:27:59 +0700

    fix: disable pwa listener

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ import 'package:ayopemilu_mobile/themes/colors_theme.dart';
 import 'package:flutter_web_frame/flutter_web_frame.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:pwa_update_listener/pwa_update_listener.dart';
+// import 'package:pwa_update_listener/pwa_update_listener.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,36 +25,37 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return FlutterWebFrame(
       builder: (context) {
-        return PwaUpdateListener(
-          onReady: () {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Row(
-                  children: [
-                    const Expanded(child: Text('Ada update tersedia')),
-                    TextButton(
-                      onPressed: () {
-                        reloadPwa();
-                      },
-                      child: const Text('Update'),
-                    ),
-                  ],
-                ),
-                duration: const Duration(days: 365),
-                behavior: SnackBarBehavior.floating,
-              ),
-            );
-          },
-          child: GetMaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'Ayo Pemilu',
-            theme: ThemeColor().themeData,
-            initialBinding: LoginBinding(),
-            home: const LoginPage(),
-            getPages: AppPages.pages,
-            builder: EasyLoading.init(),
-          ).animate().fadeIn(duration: 400.ms),
-        );
+        // return PwaUpdateListener(
+        //   onReady: () {
+        //     ScaffoldMessenger.of(context).showSnackBar(
+        //       SnackBar(
+        //         content: Row(
+        //           children: [
+        //             const Expanded(child: Text('Ada update tersedia')),
+        //             TextButton(
+        //               onPressed: () {
+        //                 reloadPwa();
+        //               },
+        //               child: const Text('Update'),
+        //             ),
+        //           ],
+        //         ),
+        //         duration: const Duration(days: 365),
+        //         behavior: SnackBarBehavior.floating,
+        //       ),
+        //     );
+        //   },
+        // child: GetMaterialApp(
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Ayo Pemilu',
+          theme: ThemeColor().themeData,
+          initialBinding: LoginBinding(),
+          home: const LoginPage(),
+          getPages: AppPages.pages,
+          builder: EasyLoading.init(),
+        ).animate().fadeIn(duration: 400.ms);
+        // );
       },
       maximumSize: const Size(475.0, 812.0),
       backgroundColor: Colors.grey,
